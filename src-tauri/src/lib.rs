b74d87f9b9b5390693a62259163e8b9b5a90f159
rahanahu <rahanahu@gmail.com>
2025-08-04 04:07:25 +0900

    tauri

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,27 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![my_custom_command, hello_from_rust])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn my_custom_command() {
+  println!("I was invoked from JavaScript!");
+}
+
+#[tauri::command]
+fn hello_from_rust() -> String{
+  "Hello from Rust".into()
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { invoke } from '@tauri-apps/api/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('first-tauri');
+  test = signal('test');
+
+  constructor() {
+    console.log('App component initialized');
+    invoke('my_custom_command');
+    invoke('hello_from_rust').then((response) => {
+      console.log('Response from Rust:', response);
+      if (typeof response === 'string') {
+        console.log('Received string response:', response);
+        this.test.set(response);
+      }
+    })
+  }
 }
